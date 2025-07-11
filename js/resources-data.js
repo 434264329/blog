ff56eb56bdcd68c01d96f06edef9cb2ae893940a
Fox <69955056+434264329@users.noreply.github.com>
2025-07-11 16:02:32 +0800

    Update resources-data.js

--- js/resources-data.js
+++ js/resources-data.js
@@ -9,6 +9,14 @@ const resourcesData = {
       "url": "https://github.com/434264329",
       "icon": "github"
     },
+    {
+      "id": 2,
+      "title": "图像识别AI",
+      "name": "进入网站",
+      "description": "提取码aitx，使用前需要安装python和requirements中所需的模块",
+      "url": "https://www.123865.com/s/4vGjVv-9CrTd?",
+      "icon": "python"
+    }
   ]
 };
 
