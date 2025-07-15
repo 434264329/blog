f64d73216751cbcd873949ce9a0e9ba7011938f9
Fox <69955056+434264329@users.noreply.github.com>
2025-07-15 17:45:42 +0800

    update

--- js/software-data.js
+++ js/software-data.js
@@ -20,5 +20,21 @@ const softwareData = [
         ],
         icon: "picture/mao_qing.ico",
         category: "清理工具"
+    },
+    {
+        id: "wenjuan",
+        title: "问卷程序",
+        description: "由AI写的问卷程序，包含暗恋概率分析程序，SCL90，SDS问卷，提取码2xc9，选择里面的quiz_reader运行程序，另一个文件时编辑qb文件的，解压相应问卷的压缩包并选择文件夹以开始答题",
+        version: "1.0",
+        size: "含有多个程序",
+        downloads: [
+            {
+                name: "蓝奏云下载(Windows)",
+                url: "https://wwtq.lanzouo.com/b002v61pzc",
+                type: "exe"
+            },
+        ],
+        icon: "picture/read.ico",
+        category: "问卷"
     }
 ];
